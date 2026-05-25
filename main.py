03c5cec19cf9200e23f111c894e03fb354c4a9df
Wika1223 <u73515@student.wat.edu.pl>
2026-05-25 20:34:18 +0200

    wip: simple structure of users list

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("test")
+companies = [
+    ["Securitas", "Warszawa"],
+    ["Solid Security", "Kraków"],
+    ["Konsalnet", "Poznań"],
+    ["Juwentus", "Gdańsk"]
+]
+
+for c in companies:
+        print(f'Firma ochroniarska {c[0]} z miejscowości {c[1]}.')
