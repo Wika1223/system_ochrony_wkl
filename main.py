9b404e2f7c41fbf4c32b17a2fc7835ef1edf346e
Wika1223 <u73515@student.wat.edu.pl>
2026-05-25 21:24:04 +0200

    feat: add insert funcionality

--- main.py
+++ main.py
@@ -7,9 +7,12 @@ from system_lib.controller import *
 while True:
     print('0 - zakończ program')
     print('1 - wyświetl firmy ochroniarskie')
+    print('2 - dodaj firmę ochroniarską')
     choose = input('wybierz opcję: ')
 
     if choose == '0':
         break
     if choose == '1':
         read_data_company(companies)
+    if choose == '2':
+        add_company(companies)
